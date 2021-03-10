dfd4b34a891453f9e16530c65b6ab60772975467
Michelle <michey.asmah21@gmail.com>
2021-03-10 16:06:24 -0500

    message
    "comments and generatePassword arguments"

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function writePassword() {
 //Ask how many characters they would like in their password when the button is clicked
   var characterAmount = prompt ('How Many Characters Would You Like In Your Password?');
 
-  //Only accept characters between 8 and 128 if between 8 and 128 continue on to confirm messages if not send alert message
+//Only accept characters between 8 and 128 if between 8 and 128 continue on to confirm messages if less than 8 or greater 128 send alert message
   if (characterAmount > 8 && characterAmount < 128) {
     var includeLowercase = confirm ('Would you like lowercase characters included?');
     var includeUppercase = confirm ('Would you like special characters included?');
@@ -18,8 +18,9 @@ function writePassword() {
     alert('The Character amount must be between 8 and 128 characters.');
   };
 
+//Add characterAmount, includeLowercase, includeUppercase, includeSpecialCharacters as arguments. 
+//characterAmount validates number of characters. includeLowercase, includeUppercase, includeSpecialCharacters return as true/false.
   var generatePassword = function (characterAmount, includeLowercase, includeUppercase, includeSpecialCharcters) {
-
   }
 
 
@@ -27,7 +28,7 @@ var passwordText = document.querySelector("#password");
 
 passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
