c9fb3f25d54b40bf51fc7727b32573db6404203c
Michelle <michey.asmah21@gmail.com>
2021-03-09 23:02:17 -0500

    fixed confirm/prompts

--- script.js
+++ script.js
@@ -3,22 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //Prompts and Confirmations
+
+//Ask how many characters they would like in their password when the button is clicked
   var characterAmount = prompt ('How Many Characters Would You Like In Your Password?');
-  var includeLowercase = confirm ('Would you like lowercase characters included?');
-  var includeUppercase = confirm ('Would you like special characters included?');
-  var includeSpecialCharcters = confirm ('Would you like special characters included?');
 
-  //Only accept characters between 8 and 128 
+  //Only accept characters between 8 and 128 if between 8 and 128 continue on to confirm messages if not send alert message
   if (characterAmount > 8 && characterAmount < 128) {
-    var lowercaseprompt = confirm ('Would you like lowercase characters included?');
-    if (lowercaseprompt === true) {
-      var includelowercase = 
-    }
+    var includeLowercase = confirm ('Would you like lowercase characters included?');
+    var includeUppercase = confirm ('Would you like special characters included?');
+    var includeSpecialCharcters = confirm ('Would you like special characters included?');
 
-
-
-  
   } else {
     alert('The Character amount must be between 8 and 128 characters.');
   };
@@ -30,9 +24,9 @@ function writePassword() {
 
 
 //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+//var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+//passwordText.value = password;
 
 }
 
