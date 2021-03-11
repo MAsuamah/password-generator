0902495d71a4d0d9e6766b8c9ca2b4cd82c301f0
Michelle <michey.asmah21@gmail.com>
2021-03-11 13:19:56 -0500

    message

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ var charArrayLowHigh = function (low, high ) {
   return charArray
 };
 
+var lowercase = charArrayLowHigh (97, 122);
+var uppercase = charArrayLowHigh (65, 90);
+var numbers = charArrayLowHigh (65, 90);
+var specialCharacters = charArrayLowHigh(48, 57).concat(
+ charArrayLowHigh(58, 64)
+ ).concat(
+   charArrayLowHigh(91, 96)
+ ).concat(
+   charArrayLowHigh(123, 126)
+ );
+
+
 // This Function will loop through characters until it creates a password that meets the selected character amount and selected criteria.
 function writePassword() {
 
   var characterAmount = prompt('How many characters would you like in your password?');
-
+  
   while (characterAmount < 8 || characterAmount > 128) {
     characterAmount = prompt('Your password must be between 8 and 128 characters. How many characters would you like?');
   }
@@ -35,52 +47,39 @@ function writePassword() {
       includeSpecialCharacters = confirm('Would you like special characters in your password?');
   }
     
-  var generatePassword = function(lowercase) {
+  var generatePassword = function(characterAmount, includeUppercase, includeLowercase, includeNumbers, includeSpecialCharacters) {
 
+    var codes = []
+
+      if (includeLowercase === true) codes = codes.concat
+      (lowercase)
+      if (includeUppercase === true) codes = codes.concat
+      (uppercase)
+      if (includeNumbers === true) codes = codes.concat
+      (numbers)
+      if (includeSpecialCharacters === true) codes = codes.concat
+      (specialCharacters)
+      debugger;
+    
     var passwordCharacters = []
-
-    if (includeLowercase === true) {
-     var lowercase = charArrayLowHigh (97, 122);
-     var randomLowercase = lowercase[Math.floor(Math.random() * lowercase.length)];
-     var charCode= String.fromCharCode(randomLowercase);
-     passwordCharacters.push(charCode);
+    for (var i = 0; i < characterAmount; i++) {
+    
+      var character = codes[Math.floor(Math.random() * codes.length)]
+      passwordCharacters.push(String.fromCharCode(character))
     }
+    return passwordCharacters.join('')
+  }
 
-    if (includeUppercase === true) {
-      var uppercase = charArrayLowHigh (65, 90);
-      var randomUppercase = uppercase[Math.floor(Math.random() * uppercase.length)];
-      var charCode= String.fromCharCode(randomUppercase);
-      passwordCharacters.push(charCode);  
-    }
-
-    if (includeNumbers === true) {
-      var numbers = charArrayLowHigh (65, 90);
-      var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
-      var charCode= String.fromCharCode(randomNumbers);
-      passwordCharacters.push(charCode); 
-
-    if (includeSpecialCharacters === true) {
-      var specialCharacters = charArrayLowHigh (65, 90);
-      var randomSpecialC = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-      var charCode= String.fromCharCode(randomSpecialC);
-      passwordCharacters.push(charCode); 
-    }
+    //This  will call the generatePassword function and store it in the variable password
+    var password = generatePassword();
 
 
+    //This  will access the text area where the password will appear and store it in the variable passwordText
+    var passwordText = document.querySelector("#password");
 
 
-//This  will call the generatePassword function and store it in the variable password
-var password = generatePassword();
-
-
-//This  will access the text area where the password will appear and store it in the variable passwordText
-var passwordText = document.querySelector("#password");
-
-
-//This function will display in the text area the value from the generatePassword function
-passwordText.value = password;
-
-} //end of writepassword function
-
+    //This function will display in the text area the value from the generatePassword function
+    passwordText.value = password;
+}
 // Clicking the generate password button will start the write password function.
 generateBtn.addEventListener("click", writePassword);
