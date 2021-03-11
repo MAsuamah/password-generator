69ccd838b876b0af03a6ccc03b2b76237398955b
Michelle <michey.asmah21@gmail.com>
2021-03-10 22:39:53 -0500

    prompts

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ var generateBtn = document.querySelector("#generate");
 // This Function will loop through characters until it creates a password that meets the selected character amount and selected criteria.
 function writePassword() {
 
-  //Start off with an empty array in which the password will be generated 
-
-  var generatingCharacters = [];
-
   //Ask how many characters the person would like 
 
   var characterAmount = prompt ('How Many Characters Would You Like In Your Password?');
 
-  //If they select an appropiate amount loop through characters until the selected character ammount is reached if not alert to choose appropiate character amount
-
+  //Prompts to optain values that wil be used in the generatePassword function
   if (characterAmount > 8 && characterAmount < 128) {
+    var includeLowercase =  confirm ('Would you like lowercase characters included?');
+    var includeUppercase =  confirm ('Would you like lowercase characters included?');
+    var includeNumbers = confirm ('Would you like numbers included in you password?');
+    var includeSpecialCharacters = confirm ('Would you like special characters in your password?');
 
+    } else  {
+      alert ('Your password must be between 8 and 128 characters!');
+    }
+ 
   }
 
+
+  //Start off with an empty array in which the password will be generated 
+
+  var generatingCharacters = [];
 
 
   //This  will call the generatePassword function and store it in the variable password
@@ -32,7 +39,7 @@ function writePassword() {
   //This function will display in the text area the value from the generatePassword function
   passwordText.value = password;
 
-}
+} //end of writepassword function
 
 // Clicking the generate password button will start the write password function.
 generateBtn.addEventListener("click", writePassword);
